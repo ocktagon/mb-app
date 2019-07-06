2ec92038cbf40b82eb445a4233c60c0a4a5d0857
Aki Suzuki <aki@siliconsurfer.io>
2019-07-06 00:30:26 -0700

    New updates for heroku

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!^n@g0-parfzmt7g^hl4o0sz#&71dci^6rg1_ief#m+0@91#6m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
